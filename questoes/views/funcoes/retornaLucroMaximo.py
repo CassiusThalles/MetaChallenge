c09e47da07b41032d55b5a2a4ffc3adee16d4d1a
Cassius Thalles Costa Mendes <cassiustcmendes@hotmail.com>
2019-07-25 18:49:52 -0300

    erasing comments in function

--- questoes/views/funcoes/retornaLucroMaximo.py
+++ questoes/views/funcoes/retornaLucroMaximo.py
@@ -8,14 +8,4 @@ def retornaValorMaximo(input):
     lucroMaximo = 0
     contador = 0
     lucroMaximo = contaLucroMaximo(variacaoDeValor, contador, lucroMaximo)
-    # for i in range(len(variacaoDeValor)):
-    #     if contador + variacaoDeValor[i] >= contador:
-    #         contador = contador + variacaoDeValor[i]
-    #     if contador + variacaoDeValor[i] < contador:
-    #         if contador + variacaoDeValor[i] > 0:
-    #             contador = contador + variacaoDeValor[i]
-    #         else:
-    #             contador = 0
-    #     if contador > lucroMaximo:
-    #         lucroMaximo = contador
     return lucroMaximo
